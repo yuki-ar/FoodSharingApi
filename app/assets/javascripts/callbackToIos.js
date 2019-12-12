33e53da3273f0e1c87c8eb7e4f850bdc74605510
yuki-ar <yuukiarai19970524@gmail.com>
2019-12-12 23:24:48 +0900

    add data属性に住所

--- app/assets/javascripts/callbackToIos.js
+++ app/assets/javascripts/callbackToIos.js
@@ -12,16 +12,39 @@ $(function () {
     false
   })
   
+ 
+
 });
 window.returnTitle = function () {
   var iosTitle = document.getElementsByClassName("hiddenIosTitle")[0].innerText;
   return iosTitle;
 }
 
+// window.returnAdress = function () {
+//   arrayAdress = []
+//   $(".shop_item-p").each(function () {
+//     arrayAdress.push($(this).text())
+//   })
+//   return arrayAdress
+// }
+window.sendDistanceText = function () {
+  $(".shop_item-p").each(function (index) {
+    // var adress = { [index]: $(this).data('adress') }
+    var adress = JSON.stringify({ [index]: $(this).data('adress') })
+    distance = window.webkit.messageHandlers.distance.postMessage(adress)
+    $(this).text()
+  })
+}
+
 window.scrollTop = function () {
   window.scrollTo(0, 0);
 }
 
+
 window.onload = function () {
   returnTitle();
+}
+
+window.onload = function () {
+  sendDistanceText();
 }
